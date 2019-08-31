82a87db43c4d826d4de739fe6d8800093cf98a74
kqq <971340511@qq.com>
2019-08-31 12:00:36 +0800

    create

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# 使用指南
+
+## 运行
+ionic serve
+
+## 打包
+ionic build --prod
+
+## 新建组件
+ionic generate
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,66 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public inputMoney = '';
+  public isInput = true;
+  constructor() {
+
+  }
+
+
+  public numberInput(item: string) {
+    console.log('btn click', item);
+    console.log('小数', /[0-9]*$/g.exec(this.inputMoney)[0].length);
+    if (this.inputMoney.length >= 15) {
+      return;
+    }
+    if (this.inputMoney.indexOf('.') === -1) {
+      this.inputMoney = this.inputMoney + item;
+    } else {
+      if (/[0-9]*$/g.exec(this.inputMoney)[0].length < 2) {
+        this.inputMoney = this.inputMoney + item;
+      }
+    }
+  }
+
+  /**
+   * floatInput
+   */
+  public floatInput(item: string) {
+    console.log(this.inputMoney.indexOf('.'));
+    if (this.inputMoney.indexOf('.') === -1) {
+      if (this.inputMoney.trim().length === 0) {
+        this.inputMoney = '0';
+      }
+      this.inputMoney = this.inputMoney + item;
+    } else {
+      // 错误 alert
+    }
+  }
+
+  /**
+   * confirmClick
+   */
+  public confirmClick() {
+    if (this.inputMoney.indexOf('.') === this.inputMoney.length - 1 ) {
+      this.inputMoney = this.inputMoney.slice(0, -1);
+    }
+    console.log(this.inputMoney);
+
+  }
+
+  /**
+   * backSpace
+   */
+  public backSpace() {
+    this.inputMoney = this.inputMoney.slice(0, -1);
+  }
+
+  /**
+   * add mode
+   */
+  public addMode() {
+
+  }
 
 }
